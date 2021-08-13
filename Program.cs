e5bfc9bbc44b3c662d9e06b3a63268cf47e583a6
JensDeNijs <jensdenijs@hotmail.com>
2021-08-13 16:18:41 +0200

    HelloWorld in cs

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pickName;
+            int pickAge;
+
+
+            Console.WriteLine("What is your name");
+
+            pickName = Console.ReadLine();
+
+            Console.WriteLine("hi " + pickName + ", what is the amount of years you have?");
+
+            pickAge = Convert.ToInt32(Console.ReadLine());
+
+            if (pickAge > 25)
+            {
+                Console.WriteLine(pickAge + " is way to many years to have.");
+            }
+            else
+            {
+                Console.WriteLine(pickAge + " is a good amount of years to have.");
+            }
+        }
+    }
+}
